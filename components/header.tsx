c377ecb19fc3117418da74e99de9e23baf87547a
Jesse Pinheiro <jessebpinheiro@hotmail.com>
2023-10-29 15:43:14 -0300

    link next

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,14 @@
 import Image from "next/image";
 import { Page } from "../models";
+import Link from "next/link";
 
-const isGithubActionsPages = process.env.GITHUB_ACTIONS_PAGES || false;
-let assetPrefix = '';
-if (isGithubActionsPages) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-}
+
 
 export default function Header({ menu }: { menu: Page[] }) {
   return (
     <header className="text-center">
       <div>
-        <a href="/">
+        <Link href="/">
           <Image
             src="./logo-infinity-white.svg"
             width={330}
@@ -20,18 +16,18 @@ export default function Header({ menu }: { menu: Page[] }) {
             alt="Infinity"
             className="py-8 m-auto"
           />
-        </a>
+        </Link>
       </div>
       <nav className="absolute z-40 w-screen shadow-[inset_0px_20px_20px_-20px_#000] text-lg">
         <ul className="grid grid-cols-5 bg-midnight-900/70 m-0 p-0">
           {menu.map((item) => (
             <li key={item.uri} className="text-center">
-              <a
-                href={assetPrefix + item.uri}
+              <Link
+                href={item.uri}
                 className="flex justify-center items-center h-16 hover:bg-midnight-950 transition duration-500 hover:ease-in-out"
               >
                 {item.title}
-              </a>
+              </Link>
             </li>
           ))}
         </ul>

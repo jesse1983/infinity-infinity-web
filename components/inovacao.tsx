ccfaff1033397e9e2c565fb57154d784453c326c
jivore78 <jivore78@gmail.com>
2023-11-03 14:13:40 -0300

    console.log removido

--- components/inovacao.tsx
+++ components/inovacao.tsx
@@ -1,5 +1,4 @@
 export default function Inovacao({ image, alt, content }) {
-  console.log(image);
   return (
     <div className="w-auto flex flex-col items-center text-center">
       <div className="bg-white flex justify-center items-center rounded-full mb-4 p-2 w-36 h-36">

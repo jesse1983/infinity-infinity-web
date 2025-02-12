0b3231b21ddaf346378a614dcf66870813ef5fc8
Jesse Pinheiro <jessebpinheiro@hotmail.com>
2025-02-11 22:57:25 -0300

    login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,22 @@ import "../styles/index.css";
 import "./mouse-trail.css";
 import "./shine.css";
 import "./additional.css";
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+
 
 const montSerrat = localFont({ src: '../public/Montserrat-VariableFont.ttf' });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const currentPath = router.pathname;
+    if (currentPath != '/login' && !localStorage.getItem('logged')) {
+      router.push('/login');
+    }
+    console.log(currentPath);
+  })
+
   return (
     <>
       <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
--- pages/login.tsx
+++ pages/login.tsx
@@ -4,6 +4,8 @@ import Layout from "../components/layout";
 import { allSettings, getPage } from "../lib/api";
 import { Settings, Page } from "../models";
 import LogoWhite from "../public/logo-infinity-white.svg";
+import { useState } from "react";
+import { useRouter } from 'next/router';
 
 type indexType = {
   generalSettings: Settings;
@@ -13,6 +15,22 @@ type indexType = {
 };
 
 export default function Index({ generalSettings, page, preview }: indexType) {
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (username === "admin" && password === "caisbyor") {
+      localStorage.setItem('logged', 'true');
+      router.push("/");
+      return;
+    }
+    alert("Usuário ou senha incorretos");
+  }
+
+
   return (
     <Layout preview={preview}>
       <Head>
@@ -38,19 +56,21 @@ export default function Index({ generalSettings, page, preview }: indexType) {
             </div>
           </div>
           {/* inputs & lembre-me*/}
-          <form className="text-left">
+          <form className="text-left" onSubmit={submit}>
             <div className="flex flex-col text-left items-center leading-tight text-xl border-x-[1px] pt-5">
               <label className="mb-2 w-3/4">Usuário:</label>
               <input
                 className="bg-night w-3/4 border-b-[1px] rounded-t-lg py-1 mb-8"
                 type="text"
                 name="user"
+                onInput={(e) => setUsername(e.currentTarget.value)}
               />
               <label className="w-3/4 mb-2">Senha:</label>
               <input
                 className="bg-night w-3/4 border-b-[1px] rounded-t-lg py-1 mb-2"
-                type="text"
+                type="password"
                 name="password"
+                onInput={(e) => setPassword(e.currentTarget.value)}
               />
               <div className="flex items-center w-3/4 mb-8">
                 <input type="checkbox" />
@@ -59,7 +79,7 @@ export default function Index({ generalSettings, page, preview }: indexType) {
             </div>
             {/* botão login */}
             <div className="flex justify-center uppercase border-white border border-t-0 w-full rounded-b-3xl">
-              <button className="bg-dusk w-3/4 rounded-md py-1 mb-10 uppercase">
+              <button type="submit" className="bg-dusk w-3/4 rounded-md py-1 mb-10 uppercase">
                 Entrar
               </button>
             </div>
